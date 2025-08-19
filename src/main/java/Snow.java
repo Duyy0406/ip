cc9454a0612663afa06def0a64edf5d0753c2e06
Duyy0406 <baoduytran01042006@gmail.com>
2025-08-19 16:36:07 +0800

    Level-1

--- src/main/java/Snow.java
+++ src/main/java/Snow.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class Snow {
     private static final String NAME = "Snow";
 
     private static void line() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void say(String cmd) {
+        line();
+        System.out.println("    " + cmd);
+        line();
+    }
+
+    private static void handle_input() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                return;
+            }
+            else {
+                say(input);
+            }
+        }
     }
     public static void main(String[] args) {
-        line();
-        System.out.println(" Hello! I'm " + NAME);
-        System.out.println(" What can I do for you?");
-        line();
-        System.out.println(" Bye. Hope to see you again soon!");
-        line();
+        say(" Hello! I'm " + NAME + "\n     What can I do for you?");
+        handle_input();
+        say(" Bye! Stay cool and see you again soon!");
     }
 }
